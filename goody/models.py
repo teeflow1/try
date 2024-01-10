e3e4da39835c28d4d8b24081c15deeb32d8c6e21
-add <temtopeayobami@gmail.com>
2024-01-10 13:25:21 +0100

    Done

--- goody/models.py
+++ goody/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    
