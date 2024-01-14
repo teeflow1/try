21710e449b482fec637efed9460d702bb8ae0e16
-add <temtopeayobami@gmail.com>
2024-01-14 15:50:50 +0100

    Done

--- goody/views.py
+++ goody/views.py
@@ -4,6 +4,25 @@ from django.core.mail import send_mail
 
 
 def home(request):
+    
+    if request.method == 'POST':
+        fname = request.POST['fname']    
+        lname = request.POST['lname']  
+        email = request.POST['email'] 
+        subject = request.POST['subject']
+        message = request.POST['message']
+        
+        
+        send_mail(
+            
+           'message from ' + fname, #Subject
+            message, # message
+            subject, #subject
+            email, # from email
+           ['temitopeayobami995@gmail.com'], # to email
+        )
+        return render(request, 'apps/contact.html', {'fname':fname})    
+    
     return render(request, 'apps/index.html', {})
 
 def property(request):
